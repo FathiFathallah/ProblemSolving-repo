3fc6f0e476d49ae97497d5292e5332a49009bc13
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-01-19 07:11:30 +0200

    Update gcd_Naive.py

--- Greatest common divisor/gcd_Naive.py
+++ Greatest common divisor/gcd_Naive.py
@@ -1,12 +1,14 @@
 # Runtime approximately a + b.
 ## Very slow for 20 digit numbers. (IT MAY TAKES YEARS !!)
 
-def gcd(a , b):
-    best = 0
-    for i in range(1,a+b):
-        if a%i == 0 and b%i == 0:
-            best = i
-    return best
+def gcd_naive(a, b):
+    current_gcd = 1
+    for d in range(2, min(a, b) + 1):
+        if a % d == 0 and b % d == 0:
+            if d > current_gcd:
+                current_gcd = d
+
+    return current_gcd
 
 
 a = int(input())
