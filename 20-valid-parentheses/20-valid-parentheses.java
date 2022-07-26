797dacce29e1b67f61d6b3066ee0d0d44c098577
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-07-27 01:40:23 +0300

    Time: 3 ms (61.02%), Space: 42.8 MB (6.10%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
+        if(s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']') return false;
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i=0; i<s.length() ; i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') stack.push(s.charAt(i));
+            else{
+                if( stack.empty() || !isCorrespondent(stack.pop(),s.charAt(i)) ) return false;
+            }
+        }
+        if(!stack.empty()) return false;
+        return true;
+}
+    
+        public boolean isCorrespondent(char x, char y){
+            if(x == '(' && y == ')') return true;
+            if(x == '{' && y == '}') return true;
+            if(x == '[' && y == ']') return true;
+            return false;            
+        }
+}
