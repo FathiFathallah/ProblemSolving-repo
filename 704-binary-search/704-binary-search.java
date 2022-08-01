c2e6d996695c7f3eaedecb56fc6157120e1d9810
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-08-01 19:18:25 +0300

    Time: 2 ms (7.10%), Space: 54.3 MB (54.29%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+        for(int number : nums){
+            if(number == target) return i;
+            else if(number > target) break;
+            i++;
+        }
+        return -1;
+    }    
+}
