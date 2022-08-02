1953827d03207d9a94ca768d4a8232bd98f4d5e7
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-08-02 19:48:12 +0300

    Time: 0 ms (100.00%), Space: 55 MB (11.36%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,8 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
         return binarySearch(nums,target,0,nums.length -1);
-    }
-    
+    } 
     public int binarySearch(int[] nums, int target, int start, int end) {
         int mid = (start + end) / 2;
         if (nums[mid] == target) return mid;
