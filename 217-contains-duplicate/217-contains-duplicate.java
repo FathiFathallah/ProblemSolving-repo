9f2a4c7e6a67b974d84c80cff1b9409bf37bfd6d
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-09-20 01:23:24 +0300

    Time: 25 ms (28.64%), Space: 73 MB (8.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hashset = new HashSet<Integer>();
         for(Integer i: nums){
-            if(hashset.contains(i)) return true;
             hashset.add(i);
         }
-        return false;
+        if(hashset.size() == nums.length) return false;
+        return true;
     }
 }
