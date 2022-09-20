838d3d61f128aef0866643ed32c3f57d12272411
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-09-20 23:59:40 +0300

    Time: 4 ms (68.12%), Space: 69.3 MB (69.69%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -5,9 +5,9 @@ class Solution {
             data[0][trust[i][0]-1]++;
             data[1][trust[i][1]-1]++;
         }
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;i++)
             if(data[0][i] == 0 && data[1][i] ==  n-1) return i + 1;
-        }
+        
         return -1;
     }
 }
