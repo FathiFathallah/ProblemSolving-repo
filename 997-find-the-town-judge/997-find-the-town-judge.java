dcc9cc324fd01d44646eab0e819bbe5bba9ef6d9
Fathi Fathallah Ali <97547912+FathiFathallah@users.noreply.github.com>
2022-09-20 23:54:16 +0300

    Time: 14 ms (27.25%), Space: 73.8 MB (33.78%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,13 +1,12 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashSet<Integer> h1 = new HashSet<Integer>();
-        int[] arr = new int[n+1];
+        int[][] data = new int[2][n];
         for(int i = 0;i<trust.length;i++){
-            h1.add(trust[i][0]);
-            arr[trust[i][1]]++;
+            data[0][trust[i][0]-1]++;
+            data[1][trust[i][1]-1]++;
         }
-        for(int i = 1;i<=n;i++){
-            if(!h1.contains(i) && arr[i] == n-1 ) return i;
+        for(int i = 0;i<n;i++){
+            if(data[0][i] == 0 && data[1][i] ==  n-1) return i + 1;
         }
         return -1;
     }
