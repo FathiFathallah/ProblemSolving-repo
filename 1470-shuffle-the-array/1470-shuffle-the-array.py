cffb2a9b4a4182b8d6ea30e8fdd789214b588170
Fathi FathAllah Ali <97547912+FathiFathallah@users.noreply.github.com>
2023-09-11 22:12:16 +0300

    Time: 39 ms (54.14%), Space: 13.5 MB (57.14%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        l1 = nums[:n]
+        l2 = nums[n:]
+        nums = []
+        for i in range(n):
+            nums.append(l1[i])
+            nums.append(l2[i])
+        return nums
+        
